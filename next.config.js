6e839d648ac17e77cc7e2dc049233e5793ea8038
Brian Berge <brian.h.berge@gmail.com>
2025-04-07 03:07:46 -0500

    Simplify next.config.js for static HTML export compatibility

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 const path = require('path');
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const withPlugins = require("next-compose-plugins");
-const withImages = require("next-images");
-const withSass = require("@zeit/next-sass");
-const withCSS = require("@zeit/next-css");
-const webpack = require("webpack");
 const fs = require('fs');
 
 // Check if nested middleware exists and delete it
@@ -14,8 +8,8 @@ if (fs.existsSync(nestedMiddlewarePath)) {
   fs.unlinkSync(nestedMiddlewarePath);
 }
 
-module.exports = withPlugins([[withSass], [withImages], [withCSS]], {
-  output: 'export', // Enable static HTML export
+module.exports = {
+  output: 'export',
   distDir: 'out',
   trailingSlash: true,
   reactStrictMode: true,
@@ -26,79 +20,23 @@ module.exports = withPlugins([[withSass], [withImages], [withCSS]], {
     domains: ['localhost', 'images.unsplash.com'],
     unoptimized: true, // Required for static export
   },
-  webpack: (config, { dev, isServer }) => {
-    // Disable middleware error check (Next.js internal check)
-    if (isServer) {
+  // Simplified webpack config
+  webpack: (config, { isServer }) => {
+    // Load the middleware suppression if it exists
+    if (isServer && fs.existsSync('./lib/suppress-middleware-error.js')) {
       const originalEntry = config.entry;
       config.entry = async () => {
         const entries = await originalEntry();
-        // Attempt to remove middleware validation
-        if (entries['pages/_app'] && !entries['pages/_app'].includes('suppressMiddlewareError')) {
+        if (entries['pages/_app']) {
           entries['pages/_app'] = ['./lib/suppress-middleware-error.js', ...entries['pages/_app']];
         }
         return entries;
       };
     }
 
+    // Add the module resolution paths
     config.resolve.modules.push(path.resolve("./"));
-    config.optimization.minimizer = [];
-
-    config.optimization.splitChunks = {
-      cacheGroups: {
-        commons: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all'
-        }
-      }
-    };
-
-    if (!dev) {
-      config.plugins.push(
-        new OptimizeCSSAssetsPlugin({
-          cssProcessorOptions: {
-            discardComments: { removeAll: true }
-          }
-        })
-      );
-    }
-
-    config.plugins.push(
-      new webpack.DefinePlugin({
-        'process.env.NEXT_IS_SERVER': JSON.stringify(isServer.toString())
-      })
-    );
-
-    if (!isServer) {
-      config.node = {
-        fs: 'empty'
-      };
-    }
-
-    // Exclude larger imports from server bundle
-    if (isServer) {
-      const origExternals = [...config.externals];
-      config.externals = [
-        (context, request, callback) => {
-          // Exclude all node_modules for server bundle to reduce size
-          if (request.match(/^[a-zA-Z0-9@]/)) {
-            // Externalize all node_modules for server bundle
-            return callback(null, `commonjs ${request}`);
-          }
-          
-          // Call the original externals
-          if (typeof origExternals[0] === 'function') {
-            origExternals[0](context, request, callback);
-          } else {
-            callback();
-          }
-        }
-      ];
-    }
 
     return config;
-  },
-  experimental: {
-    optimizeCss: true
   }
-});
+};
